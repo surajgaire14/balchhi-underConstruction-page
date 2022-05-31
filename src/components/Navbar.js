b6a18ecba665515e8d95f19f327b6cd212c8bed4
Suraj Gaire <eriag321@gmail.com>
2022-05-31 08:01:10 +0545

    under-construction

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import image from "../images/balchhi-icon.jpg";
+import InstagramFillIcon from "remixicon-react/InstagramFillIcon";
+import FacebookBoxFillIcon from "remixicon-react/FacebookBoxFillIcon";
+
+const Navbar = () => {
+  return (
+    <div className="navBar">
+      <a href="/" className="image">
+        <img src={image} alt="balchhi" />
+      </a>
+      <div className="icons">
+        <a href="https://www.facebook.com/Balchhireosrt">
+          <i>
+            <FacebookBoxFillIcon />
+          </i>
+        </a>
+        <a href="https://www.instagram.com/balchhiresort/">
+          <i>
+            <InstagramFillIcon />{" "}
+          </i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
